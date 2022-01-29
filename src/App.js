84c2fdfb7b8be0d1a1e74224cdc197c87f22bd36
shobhit jaryal <jaryalshaurya2203@gmail.com>
2022-01-29 20:48:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import NameTag from './Components/Name-Components/NameTag';
 import './App.css';
 
+const names = [
+  {firstName: 'John',lastName: 'Doe'},
+  {firstName: 'Alex',lastName: 'Clark'},
+  {firstName: 'John',lastName: 'Jackson'},
+  {firstName: 'Sahil',lastName: 'Jaryal'},
+  {firstName: 'Shobhit',lastName: 'Jaryal'},
+]
+
 function App() {
+
+  const [name , setName] = useState(names);
+
+  const [age , setAge] = useState(10); // useState is a hook. default value is 21
+      // useState returns an array with 2 values.
+      // 1st value is the current state
+      // 2nd value is a function to update the state
+      console.log("parent")
+      const ageUp = () => {
+        setAge(p=>p+1);
+      }
+      const ageDown = () => {
+        if(age>0){
+          setAge(p=>p-1);
+        }else{
+          return;
+        }
+      }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>useState Hook</h1>
+        <h2>Age is : {age}</h2>
+        <div className="button">
+        <button className="btn1" onClick={ageUp}>Inc + </button> {/*button to increase age */}
+        <button className="btn2" onClick={ageDown}>Dec - </button> {/*button to decrease age */}
+        </div>
+
+        {
+          name.map((name,index) => {
+            return <NameTag firstName={name.firstName} lastName={name.lastName} key={index} slaveNo={index} age={age} />;  
+          })
+        }
+        {/*should not use index as key */}
       </header>
     </div>
   );
---
+++ src/Components/Name-Components/NameTag.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from "react";
+import "./NameTag-comp.css";
+ function NameTag(props) {
+   useEffect(()=>{
+    console.log("child")
+   },[]); 
+    if(props.firstName === "Shobhit"){
+        return <h1 className="vip">Hello Shobhit</h1>
+    }
+  return (
+    <div>
+      <h1 className="people" >{props.firstName} {props.lastName} <sub className="tag" >slave {props.slaveNo +1}</sub></h1>
+    </div>
+  );
+}
+//style={{color:props.firstName==='Sahil'&&props.age>=26&&"red"}}
+
+export default NameTag;
